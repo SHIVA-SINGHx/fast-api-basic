4c7f64ed85a04fe3791625b6087e37b6e8d591df
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-09-22 10:55:14 -0700

    adding create products routes

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,12 @@ app = FastAPI()
 def on_startup():
     create_db()
 
+# get products
 @app.get("/products/", response_model= list[schemas.Product])
 def get_all_products(db: Session= Depends(get_db)):
-    return {crud.get_product,"Database connected successfully"}
+    return crud.get_product(db)
+
+# create products
+@app.post("/prodcuts", response_model = list[schemas.Product])
+def create_prodcuts(product: schemas.ProductCreate, db: Session= Depends(get_db)):
+    return crud.create_product(db,product)
